8544eb85f12cd98b2276bd89c703476f15eac6c1
falak230508 <72301138+falak230508@users.noreply.github.com>
2020-12-04 22:11:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup (){
+createCanvas(1000,800);
+
+var falak = createSprite(500,400,50,50);
+falak.shapeColor = "purple";
+
+}
+function draw (){
+background("cyan");
+
+
+
+
+
+drawSprites()
+}
